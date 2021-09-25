4c11ed893034bb4d0afa83f98e6b092487738571
ErikZhouXin@EZhouXin.com <ErikZhouXin@EZhouXin.com>
2021-09-25 15:19:34 +0800

    [ErikZhouXin]简化连接池设置

--- src/SQLiteCipher/SqliteConnectionPool.cs
+++ src/SQLiteCipher/SqliteConnectionPool.cs
@@ -12,22 +12,7 @@ namespace System.Data.SQLiteCipher
     /// </summary>
     public class SqliteConnectionPool
     {
-        private static int _size = 5;
-        private static int _clearInterval = 60;
-        private static int _timeout = 5;
         private static ConcurrentDictionary<string, SqliteConnectionPool> _poolDic = new();
-        /// <summary>
-        /// 初始化线程池的大小
-        /// </summary>
-        public static Int32 Size { get => _size; set => _size = value > 2 ? value : _size; }
-        /// <summary>
-        /// 清除间隔(分钟)
-        /// </summary>
-        public static Int32 ClearInterval { get => _clearInterval; set => _clearInterval = value > 0 ? value : _clearInterval; }
-        /// <summary>
-        /// 超时时间
-        /// </summary>
-        public static Int32 LockTimeout { get => _timeout; set => _timeout = value > 2 ? value : _timeout; }
         /// <summary>
         /// 获取实例
         /// </summary>
@@ -47,7 +32,7 @@ namespace System.Data.SQLiteCipher
             // 多线程并发的时候会产生多个实例,不知为何
             //return _poolDic.GetOrAdd(connString, (k) => { return new SqliteConnectionPool(connString); });
             SqliteConnectionPool pool;
-            if (Monitor.TryEnter(_poolDic, TimeSpan.FromSeconds(LockTimeout)))
+            if (Monitor.TryEnter(_poolDic, TimeSpan.FromSeconds(5)))
             {
                 pool = _poolDic.GetOrAdd(connString, (k) => new SqliteConnectionPool(connString));
                 Monitor.Exit(_poolDic);
@@ -66,7 +51,7 @@ namespace System.Data.SQLiteCipher
         private Timer _clearTimer = new Timer
         {
             Enabled = true, // 是否执行System.Timers.Timer.Elapsed事件
-            Interval = ClearInterval * 60000, // 执行间隔时间,单位为毫秒;此时时间间隔单位为分钟
+            Interval = 1800000, // 执行间隔时间,单位为毫秒;此时时间间隔单位为分钟
             AutoReset = true, // 设置是执行一次（false）还是一直执行(true)
         };
         /// <summary>
@@ -83,15 +68,9 @@ namespace System.Data.SQLiteCipher
             // 定时清理连接池
             _clearTimer.Elapsed += (s, e) =>
             {
-                if (_connPool.Count > _size)
+                if (_connPool.TryDequeue(out SQLiteConnection curr))
                 {
-                    for (int i = 0; i < _connPool.Count - _size; i++)
-                    {
-                        if (_connPool.TryDequeue(out SQLiteConnection curr))
-                        {
-                            curr.Release();
-                        }
-                    }
+                    curr.Release();
                 }
             };
             _clearTimer.Start();
@@ -114,7 +93,7 @@ namespace System.Data.SQLiteCipher
         /// <param name="instance"></param>
         public void Recycle(SqliteConnection instance)
         {
-            if(instance is SQLiteConnection conn)
+            if (instance is SQLiteConnection conn)
             {
                 _connPool.Enqueue(conn);
             }
@@ -139,7 +118,6 @@ namespace System.Data.SQLiteCipher
             /// <param name="disposing"></param>
             protected override void Dispose(bool disposing)
             {
-                //base.Dispose(disposing);
                 GetPool(ConnectionString).Recycle(this);
             }
             /// <summary>
